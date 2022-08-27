023568ec56b0311e4214e9a7991e72ab6e933069
AnitaNikolovska <63537596+AnitaNikolovska@users.noreply.github.com>
2022-08-27 11:39:11 +0200

    added NuGet Packages

---
+++ MoviesApp/MoviesApp.DAL/MoviesAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApp.DAL
+{
+    //Microsoft.EntityFrameworkCore
+    //Microsoft.EntityFrameworkCore.Abstractions
+    //Microsoft.EntityFrameworkCore.Design
+    //Microsoft.EntityFrameworkCore.Relational
+    //Microsoft.EntityFrameworkCore.SqlServer
+    //Microsoft.EntityFrameworkCore.Tools
+    public class MoviesAppDbContext : DbContext
+    {
+        public MoviesAppDbContext(DbContextOptions option)
+               : base(option) { }
+
+        public DbSet<MovieDto> Movies { get; set; }
+    }
+}
